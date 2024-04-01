176fd6ff0da36173cea442792a38c12a5070bf24
Voopwraz <104880061+Voopwraz@users.noreply.github.com>
2024-04-02 03:28:13 +0500

    Create zhm9k_start.user.js

---
+++ zhm9k_start.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name       Жмакатель start
+// @version    0.1
+// @description  Нажимает кнопку после обновления страницы с задержкой и проверкой
+// @author      Tonyrykozhop
+// @match       https://sunflower-land.com/play/
+// @grant       none
+// ==/UserScript==
+
+(function() {
+  // ID кнопки
+  const buttonId = 'start';
+
+  // Флаг для проверки нажатия кнопки
+  let buttonClicked = false;
+
+  // Функция для нажатия кнопки
+  function clickButton() {
+    if (!buttonClicked) {
+      buttonClicked = true;
+
+      setTimeout(function() {
+        const button = document.getElementById(buttonId);
+        if (button) {
+          button.click();
+        }
+      }, 10000); // 10 секунд
+    }
+  }
+
+  // Наблюдатель за обновлениями DOM
+  const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.type === 'childList') {
+        clickButton();
+      }
+    });
+  });
+
+  // Начать наблюдение за DOM
+  observer.observe(document.body, { childList: true });
+
+  // Нажать кнопку сразу после загрузки страницы
+  clickButton();
+})();
